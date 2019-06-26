f5e3b0628af1730e86b891f3446e6657250c44f6
Whambuoy <esther25maina@gmail.com>
2019-06-26 18:56:59 +0300

    Variables

---
+++ src/exercises/Variables.java
@@ -0,0 +1,36 @@
+package exercises;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//--------Below are the 8 primitive types in Java--------/
+		
+		int myNumber;
+		short myShort = 66;
+		long myLong = 77;
+		
+		double myDouble = 88.88;
+		//You have to put 'f' at the end of the float value. Without an 'f', this is usually considered as double value
+		float myFloat = 99.67f;
+		
+		//Holds a unicode character in single quotes
+		char myChar = 'y';
+		boolean myBoolean = true;
+		
+		//Can only hold 8 bits of data
+		byte myByte = 127;
+		
+		myNumber = 55;
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+
+	}
+
+}

9b89b432ca0a44bcd150a90b7ba65922e0a52f5a
Whambuoy <esther25maina@gmail.com>
2019-07-10 18:14:38 +0300

    PAckages

---
+++ src/exercises/Packages.java
@@ -0,0 +1,13 @@
+package exercises;
+
+import ocean.Fish;
+
+public class Packages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Fish fish = new Fish();
+	}
+
+}

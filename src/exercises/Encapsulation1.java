1e4654a270df8809ca8d2c08113939adbbbe2fcd
Whambuoy <esther25maina@gmail.com>
2019-07-19 10:20:42 +0300

    Encapsulation

--- src/exercises/Encapsulation1.java
+++ src/exercises/Encapsulation1.java
@@ -1,7 +1,36 @@
 package exercises;
 
-public class Encapsulation1 {
 
+class Wambui{
+	
+	//Cannot be changed from outside the class
+	public static final int id = 19;
+	
+	private String name;
+	
+	public String getData() {
+		String data = "Some stuff" + calculateGrowthForecast();
+		return data;
+	}
+	
+	//This method is only accessible within this class
+	private int calculateGrowthForecast() {
+		return 9;
+	}
+
+	//Getters and setters are methods that allow you to access private data
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
+
+public class Encapsulation1 {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
